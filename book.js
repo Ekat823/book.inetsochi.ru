3bb4d8564eef2535b1cfb975e681f4f3461253de
Ekat <ekata823@gmail.com>
2023-01-05 20:39:44 +0300

    chpater 5 ready

--- book.js
+++ book.js
@@ -1,8 +1,29 @@
-const popUP = () => {
+const popUp = () => {
   const popup = document.getElementById("exp");
-  const popup1 = document.getElementById("exp1");
-  const popup2 = document.getElementById("exp2");
   popup.classList.toggle("show");
+}
+
+const popUpOne = () => {
+  const popup1 = document.getElementById("exp1");
   popup1.classList.toggle("show");
+}
+
+const popUpTwo = () => {
+  const popup2 = document.getElementById("exp2");
   popup2.classList.toggle("show");
+}
+
+const popUpThree = () => {
+  const popup3 = document.getElementById("exp3");
+  popup3.classList.toggle("show");
+}
+
+const popUpFour = () => {
+  const popup4 = document.getElementById("exp4");
+  popup4.classList.toggle("show");
+}
+
+const popUpFive = () => {
+  const popup5 = document.getElementById("exp5");
+  popup5.classList.toggle("show");
 }
