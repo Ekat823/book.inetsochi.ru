0e3147937119f265dc2dbccb7ea9509e0180e687
Ekat <ekata823@gmail.com>
2023-01-05 18:13:45 +0300

    chapter 4 ready

--- book.js
+++ book.js
@@ -1,4 +1,8 @@
 const popUP = () => {
   const popup = document.getElementById("exp");
+  const popup1 = document.getElementById("exp1");
+  const popup2 = document.getElementById("exp2");
   popup.classList.toggle("show");
+  popup1.classList.toggle("show");
+  popup2.classList.toggle("show");
 }
