f5da597d4fbaa2963076a47e0a3ab138406d1ebc
Ekat <ekata823@gmail.com>
2023-01-06 11:21:38 +0300

    changed everying to modal and removed popup

--- book.js
+++ book.js
@@ -1,49 +1,18 @@
-const popUp = () => {
-  const popup = document.getElementById("exp");
-  popup.classList.toggle("show");
-}
-
-const popUpOne = () => {
-  const popup1 = document.getElementById("exp1");
-  popup1.classList.toggle("show");
-}
-
-const popUpTwo = () => {
-  const popup2 = document.getElementById("exp2");
-  popup2.classList.toggle("show");
-}
-
-const popUpThree = () => {
-  const popup3 = document.getElementById("exp3");
-  popup3.classList.toggle("show");
-}
-
-const popUpFour = () => {
-  const popup4 = document.getElementById("exp4");
-  popup4.classList.toggle("show");
-}
-
-const popUpFive = () => {
-  const popup5 = document.getElementById("exp5");
-
-  popup5.classList.toggle("show");
-}
-
-
-
-const openModal = () => {
-  const modal = document.getElementById("myModal");
+const openModal = (num) => {
+  const modal = document.getElementById("exp" + num);
   modal.style.display = "block";
 }
 
-const closeModal = () => {
-  const modal = document.getElementById("myModal");
+const closeModal = (num) => {
+  const modal = document.getElementById("exp" + num);
   modal.style.display = "none";
 }
 
 window.onclick = function (event) {
-  const modal = document.getElementById("myModal");
-  if (event.target == modal) {
-    modal.style.display = "none";
+  const modal = document.getElementsByClassName("modal");
+  for (let i = 0; i < modal.length; i++) {
+    if (event.target == modal[i]) {
+      modal[i].style.display = "none";
+    }
   }
 }
